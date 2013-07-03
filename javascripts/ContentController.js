8c959ed4a9d3b4451c83451f50767cb3b148cc91
Eric <Ericrius1@gmail.com>
2013-07-02 22:45:48 -0700

    changed relative linekd in link

--- javascripts/ContentController.js
+++ javascripts/ContentController.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-  console.log("I am ready");
+$(document).ready(function() {
   $('#container').hide();
+  var opacityTarget = 0.06
 
-  $('#name').on('click', function(){
-    $('#container').fadeToggle('slow');
-    $('#canvas').fadeToggle('slow');
+  $('#name').on('click', function() {
+    $('#container').fadeToggle(1000);
+    $('#canvas').animate({
+      opacity: opacityTarget,
+    }, 1000, function() {
+      opacityTarget = 1.0;
+    });
+
   })
 })
