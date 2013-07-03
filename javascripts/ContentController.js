fe0fddfc4dbcb22ea6da08344c34ad63a3351e81
Eric <Ericrius1@gmail.com>
2013-07-02 23:30:59 -0700

    animating properly

--- javascripts/ContentController.js
+++ javascripts/ContentController.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   $('#container').hide();
-  var opacityTarget = 0.06
+  var opacityTarget = 0.08;
 
   $('#name').on('click', function() {
     $('#container').fadeToggle(1000);
-    $('#canvas').fadeToggle(1000);
+    console.log(opacityTarget);
+    $('#canvas').animate({
+      opacity: opacityTarget,
+    }, 1000, function() {
+      opacityTarget = opacityTarget === 1.0 ? 0.06 : 1.0;
+      console.log(opacityTarget);
 
-
-  })
+    });
+    
+  });
 })
